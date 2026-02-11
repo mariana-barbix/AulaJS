0b8322c6767f8a9213cffeb610aa789f67a40f7b
mariana-barbix <marianabarbi39@gmail.com>
2026-02-11 15:04:17 -0300

    11/02

---
+++ aula01/script.js
@@ -0,0 +1,18 @@
+var nome = "Mariana";
+
+var idade = 16;
+
+var altura = 1.63;
+
+console.log ( nome );
+console.log( idade );
+console.log( altura);
+
+document.write("<h1>Olá, seja bem vindo ao JavaSript</h1>")
+
+document.write( nome + "<br>" );
+document.write( idade + "<br>");
+document.write( altura + "<br>"); 
+
+console.log(idade * 365);
+document.write("<p style='color=red'>Você já viveu" + idade *365</p> );
